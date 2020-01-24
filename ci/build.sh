19e5cd4bcdc37da88f1c974bd6e6e35b95d8c527
Antoine Albertelli <antoinealb@google.com>
2020-01-24 14:23:31 +0100

    Enable CI builds for computer vision

--- ci/build.sh
+++ ci/build.sh
@@ -59,6 +59,14 @@ case $BUILD_TYPE in
         popd
         ;;
 
+    computer-vision)
+        mkdir -p computer-vision/build
+        pushd computer-vision/build
+        cmake ..
+        make all
+        popd
+        ;;
+
     *)
         echo "Unknown build type $BUILD_TYPE"
         exit 1
